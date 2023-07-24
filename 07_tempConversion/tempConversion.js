c451778b8b656ff9c0c31115e386496ca010ff4a
sgbbco <116119784+sgbbco@users.noreply.github.com>
2023-07-23 23:46:55 -0600

    Solved exercises 01 - 08

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,8 +1,16 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  let celsius = Math.round((fahrenheit - 32) * (5 / 9) * 10) / 10;
+  return celsius;
 };
 
-const convertToFahrenheit = function() {
+console.log(convertToCelsius(-10)) 
+
+const convertToFahrenheit = function(celsius) {
+  let fahrenheit = Math.round(((celsius * 9) / 5 + 32) * 10) / 10;
+  return fahrenheit;
 };
+
+console.log(convertToFahrenheit(-10)) 
 
 // Do not edit below this line
 module.exports = {
--- 08_calculator/calculator.js
+++ 08_calculator/calculator.js
@@ -1,25 +1,34 @@
-const add = function() {
-	
+const add = function(num1, num2) {
+  return num1 + num2;	
 };
 
-const subtract = function() {
-	
+const subtract = function(num1, num2) {
+  return num1 - num2;	
 };
 
-const sum = function() {
-	
+const sum = function(array) {
+  return array.reduce(function(total, num) {
+    return total + num;
+  }, 0);
 };
 
-const multiply = function() {
 
+const multiply = function(...numbers) {
+  return numbers.reduce((product, num) => product * num, 1);
 };
 
-const power = function() {
-	
+
+const power = function(num1, num2) {
+  return Math.pow(num1, num2);
 };
 
-const factorial = function() {
-	
+const factorial = function(num1) {
+  if (num1 === 0) return 1;
+  let product = 1;
+  for (let i = num1; i > 0; i--) {
+    product *= i;
+  }
+  return product;
 };
 
 // Do not edit below this line
